d8cad1b9464fbc517bacd413dd62f782c663c4bc
agabet <aurelien.gabet@gmail.com>
2013-04-30 16:41:12 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cocktailizr::Application.config.secret_token = 'dba8d0c1e65c614e355672be6a426ddb226afd0f5836e3dcdfff7b2a7a30fe9d6bd369874089e665a8a97aaa565d7e08ac8f9742cf77bd200245456412554345'
